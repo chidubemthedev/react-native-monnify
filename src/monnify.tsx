818a947a6ed3deb94d467caa01e05effbcb853a3
charles.onuorah <charles.onuorah@teamapt.com>
2026-09-22 12:34:50 +0100

    Fix webview popup block

--- src/monnify.tsx
+++ src/monnify.tsx
@@ -89,7 +89,8 @@ const Monnify: React.FC<MonnifyProps> = ({
           javaScriptEnabled
           domStorageEnabled
           startInLoadingState
-          setSupportMultipleWindows={true}
+          setSupportMultipleWindows
+          javaScriptCanOpenWindowsAutomatically
           allowFileAccess
           allowUniversalAccessFromFileURLs
           mixedContentMode="always"
